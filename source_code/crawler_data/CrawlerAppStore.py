05b0e5bb6da6eb1990e0cb03af5fd5f08bf14fdf
Tran Ngoc Bao <tranngocbaooooo12062003@gmail.com>
2023-12-08 16:41:01 +0700

    add code crawler data and data !

--- source_code/crawler_data/CrawlerAppStore.py
+++ source_code/crawler_data/CrawlerAppStore.py
@@ -38,6 +38,8 @@ def crawlContents(fileName, i):
 
         data = {}
 
+        data['link'] = listLink[j]
+
         if len(nameAndAge) == 0:
             data['nameAndAge'] = ""
         else:
@@ -62,46 +64,7 @@ def crawlContents(fileName, i):
 
     setupFile(fileName, True)
 
-def crawlContentsSpecial():
-    setupFile("appstore_195_197.json", False)
-    deli = ""
-
-    for j in range(195, 198):
-        link = requests.get(listLink[j])
-        soup = BeautifulSoup(link.content, 'html.parser')
-        nameAndAge = soup.find_all(class_ = 'app-header__title')
-        describe = soup.find_all('a', class_ = 'inline-list__item')
-        rating = soup.find_all(class_ = 'we-rating-count')
-        information = soup.find_all('dd', class_ = 'information-list__item__definition')
-
-        data = {}
-
-        if len(nameAndAge) == 0:
-            data['nameAndAge'] = ""
-        else:
-            data['nameAndAge'] = nameAndAge[0].text
-
-        if len(describe) == 0:
-            data['describe'] = ""
-        else:
-            data['describe'] = describe[0].text
-
-        if len(rating) == 0:
-            data['rating'] = ""
-        else:
-            data['rating'] = rating[0].text
-
-        data['company'] = information[0].text
-        data['size'] = information[1].text
-        data['price'] = information[7].text
-
-        writeFile("appstore_195_197.json", data, deli)
-        deli = ",\n"
-
-    setupFile("appstore_195_197.json", True)
-
 if __name__ == "__main__":
-    for i in range(36, 39):
+    for i in range(39):
         fileName = "appstore_" + str(i * 5) + "_" + str(i * 5 + 4) + ".json"
         crawlContents(fileName, i)
-    crawlContentsSpecial()
