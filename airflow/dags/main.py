8845109798e8eba63efdbcab0cbfcb192e7d0dcc
Tran Ngoc Bao <tranngocbaooooo12062003@gmail.com>
2024-08-23 23:39:44 +0700

    Update docker-compose

--- airflow/dags/main.py
+++ airflow/dags/main.py
@@ -1,13 +1,11 @@
-import json, requests
-import redis
 from airflow import DAG
-from airflow.operators.python_operator import PythonOperator
+from airflow.operators.bash_operator import BashOperator # type: ignore
 from datetime import datetime, timedelta
 
 default_args = {
     "owner": "airflow",
     "depends_on_past": False,
-    "start_date": datetime(2024, 5, 1),
+    "start_date": datetime(2024, 8, 1),
     "email": ["airflow@airflow.com"],
     "email_on_failure": False,
     "email_on_retry": False,
@@ -21,21 +19,24 @@ default_args = {
 
 dag = DAG("main", default_args = default_args, schedule_interval = timedelta(30))
 
-def crawl_app_store():
-    main_page = requests.get("https://apps.apple.com/vn/genre/ios-tr%C3%B2-ch%C6%A1i/id6014?l=vi")
-    soup = BeautifulSoup(main_page.content.decode("utf-8"), "html.parser")
-    list = soup.find_all('a')
-    client = InsecureClient('hdfs://namenode:9000', user='root')
-    for i in list:
-        if i['href'].find('/vn/app/') != -1:
-            game = requests.get(i['href']).content
-            client.write("app_store", game)
-
-crawl_app_store = PythonOperator(
+crawl_app_store = BashOperator(
     task_id = "crawl_app_store",
-    python_callable = crawl_app_store,
-    provide_context = True,
+    bash_command = "python3 /usr/local/airflow/code/hello.py",
     dag = dag,
 )
 
+# crawl_google_play_mobile = BashOperator(
+#     task_id = "crawl_google_play_mobile",
+#     bash_command = "ls -la",
+#     dag = dag,
+# )
+
+# crawl_google_play_tablet = BashOperator(
+#     task_id = "crawl_google_play_tablet",
+#     bash_command = "ls -la",
+#     dag = dag,
+# )
+
 crawl_app_store
+# crawl_google_play_mobile
+# crawl_google_play_tablet
