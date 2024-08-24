3af3d6210c96b4d2025f0bcac403e15940f88fc4
Tran Ngoc Bao <tranngocbaooooo12062003@gmail.com>
2024-08-24 21:25:32 +0700

    Demo crawl data

--- airflow/dags/main.py
+++ airflow/dags/main.py
@@ -1,42 +1,41 @@
-from airflow import DAG
-from airflow.operators.bash_operator import BashOperator # type: ignore
-from datetime import datetime, timedelta
+import pendulum
+from airflow.decorators import dag, task
+import requests
+from bs4 import BeautifulSoup
+import redis
 
-default_args = {
-    "owner": "airflow",
-    "depends_on_past": False,
-    "start_date": datetime(2024, 8, 1),
-    "email": ["airflow@airflow.com"],
-    "email_on_failure": False,
-    "email_on_retry": False,
-    "retries": 1,
-    "retry_delay": timedelta(minutes=5),
-    # "queue": "bash_queue",
-    # "pool": "backfill",
-    # "priority_weight": 10,
-    # "end_date": datetime(2016, 1, 1),
-}
-
-dag = DAG("main", default_args = default_args, schedule_interval = timedelta(30))
-
-crawl_app_store = BashOperator(
-    task_id = "crawl_app_store",
-    bash_command = "python3 /usr/local/airflow/code/hello.py",
-    dag = dag,
+@dag(
+    schedule = None,
+    start_date = pendulum.datetime(2024, 8, 1, tz="UTC"),
+    catchup = False,
+    tags = ["example"],
 )
+def tutorial_taskflow_api():
+    @task()
+    def extract_load_redis():
+        req = requests.get("https://apps.apple.com/vn/genre/ios-tr%C3%B2-ch%C6%A1i/id6014?l=vi")
+        t = req.text
+        soup = BeautifulSoup(t, "html.parser")
+        l = soup.find_all('a')
+        output = ""
+        for i in l:
+            if str(i).find("/vn/app/") != -1:
+                output += i["href"]
+        r = redis.Redis(host = "redis", port = 6379, db = 0)
+        r.set("test", output)
+        
+    
+    # @task(multiple_outputs = True)
+    # def transform(order_data_dict: dict):
+    #     total_order_value = 0
+    #     for value in order_data_dict.values():
+    #         total_order_value += value
+    #     return {"total_order_value": total_order_value}
+    
+    # @task()
+    # def load(total_order_value: float):
+    #     print(f"Total order value is: {total_order_value:.2f}")
 
-# crawl_google_play_mobile = BashOperator(
-#     task_id = "crawl_google_play_mobile",
-#     bash_command = "ls -la",
-#     dag = dag,
-# )
+    order_data = extract_load_redis()
 
-# crawl_google_play_tablet = BashOperator(
-#     task_id = "crawl_google_play_tablet",
-#     bash_command = "ls -la",
-#     dag = dag,
-# )
-
-crawl_app_store
-# crawl_google_play_mobile
-# crawl_google_play_tablet
+tutorial_taskflow_api()
